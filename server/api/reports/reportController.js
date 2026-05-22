daf40f8e5c78fec9da0990b0ddce802307259065
Manrit Kaur <manrit.cr26@gmail.com>
2026-05-22 10:33:59 +0530

    Completed Report APIs
    Report APIs - completed and tested

--- server/api/reports/reportController.js
+++ server/api/reports/reportController.js
@@ -54,7 +54,9 @@ const createReport = async (req, res) => {
 // API to get all reports
 const getAllReports = async (req, res) => {
   try {
-    const reports = await Report.find();
+    const reports = await Report.find()
+      .populate("business_id", "name city")
+      .populate("user_id", "name email");
 
     return res.status(200).json({
       message: "Fetched all reports",
@@ -69,18 +71,130 @@ const getAllReports = async (req, res) => {
 };
 
 // API to get report by ID
-// const getReportById = async (req, res ) => {
-//   try {
-//     const { id } = 
-//   } catch (error) {
-//     return res.status(500).json({
-//       message : error.message
-//     })
-//   }
-// }
+const getReportById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        message: "Report ID is required",
+      });
+    }
+    const report = await Report.findById(id)
+      .populate("business_id", "name city")
+      .populate("user_id", "name email");
+    if (!report) {
+      return res.status(404).json({
+        message: "Report Not Found",
+      });
+    }
+    return res.status(200).json({
+      message: "Fetched Report by ID",
+      data: report,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
+// API to approve Report
+const approveReport = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        message: "Report ID is required",
+      });
+    }
+    const report = await Report.findById(id)
+      .populate("business_id", "name city")
+      .populate("user_id", "name email");
+    if (!report) {
+      return res.status(404).json({
+        message: "Report Not Found",
+      });
+    }
+    report.status = "approved";
+    await report.save();
+
+    return res.status(200).json({
+      message: "Report Approved",
+      data: report,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// API to reject the report
+const rejectReport = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        message: "Report ID is required",
+      });
+    }
+    const report = await Report.findById(id)
+      .populate("business_id", "name city")
+      .populate("user_id", "name email");
+    if (!report) {
+      return res.status(404).json({
+        message: "Report Not Found",
+      });
+    }
+    report.status = "rejected";
+    await report.save();
+
+    return res.status(200).json({
+      message: "Report Approved",
+      data: report,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// API to get Business's reports
+const getBusinessReports = async (req, res) => {
+  try {
+    const { b_id } = req.body;
+    if (!b_id) {
+      return res.status(400).json({
+        message: "Business ID is required",
+      });
+    }
+    const reports = await Report.find({ business_id: b_id })
+      .populate("business_id", "name city")
+      .populate("user_id", "name email");
+    if (!reports) {
+      return res.status(404).json({
+        message: "Reports Not Found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Fetched Business's Reports",
+      count: reports.length,
+      data: reports,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   createReport,
-  getAllReports
+  getAllReports,
+  getReportById,
+  approveReport,
+  rejectReport,
+  getBusinessReports,
 };
--- server/routes/report.routes.js
+++ server/routes/report.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { checkUser, verifyToken, isAdmin } = require("../middleware/auth.middleware");
-const { createReport, getAllReports } = require("../api/reports/reportController");
+const { createReport, getAllReports, getReportById, approveReport, rejectReport, getBusinessReports } = require("../api/reports/reportController");
 const router = express.Router();
 
 router.post("/createreport", verifyToken, checkUser, createReport);
 router.post("/getallreports", verifyToken, checkUser, isAdmin, getAllReports);
+router.post("/getreportbyid", verifyToken, checkUser, isAdmin , getReportById)
+router.post("/approvereport", verifyToken, checkUser, isAdmin , approveReport)
+router.post("/rejectreport", verifyToken, checkUser, isAdmin , rejectReport)
+router.post("/getbusinessreports", verifyToken, checkUser, isAdmin , getBusinessReports)
 
 module.exports = router;
